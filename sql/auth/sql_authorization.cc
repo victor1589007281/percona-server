aafa6b0a7fe062a7cf8c99507a69673e2bcb78dc
victorliang <victorliang@tencent.com>
2024-12-09 00:17:30 +0800

    start build fix

--- sql/auth/sql_authorization.cc
+++ sql/auth/sql_authorization.cc
@@ -7536,6 +7536,10 @@ bool operator==(const Role_id &a, const std::string &b) {
   return tmp == b;
 }
 
+bool operator==(const Role_id &a, const LEX_CSTRING &b) {
+      return a == to_string(b);
+}
+
 bool operator==(const std::pair<Role_id, bool> &a, const std::string &b) {
   return a.first == b;
 }
